597b93000d9fd38bb3b5165a1d7c71e6e389ba97
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-06 13:13:50 +0200

    Create wrapper for Categories & Attributes

---
+++ olx/categories_attributes.py
@@ -0,0 +1,39 @@
+from .olx import Olx
+from .models import Category, CategoryAttribute, CategorySuggestion
+from dacite import from_dict
+from typing import List
+
+
+class CategoriesAttributes(Olx):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def get_categories(self) -> List[Category]:
+        endpoint = self.endpoints["categories_attributes"]["get_categories"]
+        response = self.get(endpoint)
+        data = response.json()["data"]
+        return [from_dict(Category, obj) for obj in data]
+
+    def get_category(self, category_id: int) -> Category:
+        endpoint = self.endpoints["categories_attributes"]["get_category"].format(
+            id=category_id
+        )
+        response = self.get(endpoint)
+        data = response.json()["data"]
+        return from_dict(Category, data)
+
+    def get_category_attributes(self, category_id: int) -> List[CategoryAttribute]:
+        endpoint = self.endpoints["categories_attributes"][
+            "get_category_attributes"
+        ].format(id=category_id)
+        response = self.get(endpoint)
+        data = response.json()["data"]
+        return [from_dict(CategoryAttribute, obj) for obj in data]
+
+    def get_category_suggestions(self, ad_title: str) -> List[CategorySuggestion]:
+        endpoint = self.endpoints["categories_attributes"]["get_category_suggestions"]
+        params = {"q": ad_title}
+        response = self.get(endpoint, params=params)
+        data = response.json()["data"]
+        print(data)
+        return [from_dict(CategorySuggestion, obj) for obj in data]
