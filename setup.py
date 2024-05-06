ba7582c225241f0352806a49fc2afc7939918291
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-06 21:27:36 +0200

    Add setup configuration

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="olx-api-wrapper",
+    version="0.1.0",
+    description="Unofficial Wrapper for OLX API",
+    author="Paweł Stawikowski",
+    author_email="pawikoski@gmail.com",
+    packages=["olx"],
+    url="https://github.com/Pawikoski/olx-api-wrapper",
+    install_requires=["requests", "dacite"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
