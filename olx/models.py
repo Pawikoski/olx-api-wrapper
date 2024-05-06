6b5b2bc0308170d07dde503041aeffc886b47947
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-06 11:56:41 +0200

    Add Users models

--- olx/models.py
+++ olx/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
@@ -7,4 +8,34 @@ class AuthResponse:
     expires_in: int
     token_type: str
     scope: str
-    refresh_token: str | None = None
+    refresh_token: Optional[str]
+
+
+@dataclass
+class UsersMe:
+    id: int
+    email: str
+    status: str
+    name: str
+    phone: Optional[str]
+    phone_login: Optional[int]
+    created_at: str
+    last_login_at: str
+    avatar: Optional[str]
+    is_business: bool
+
+
+@dataclass
+class UsersGetUser:
+    id: int
+    name: str
+    avatar: Optional[str]
+
+
+@dataclass
+class UsersAccountBalance:
+    sum: float
+    wallet: float
+    bonus: float
+    refund: float
+    currency: Optional[str]
