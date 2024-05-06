7be78b405d9c0f19866acafe601af6bb7eac82e0
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-06 13:13:57 +0200

    Import CategoriesAttributes

--- olx/__init__.py
+++ olx/__init__.py
@@ -2,3 +2,4 @@ from .auth import Auth
 from .users import Users
 from .cities_districts import CitiesDistricts
 from .languages_currencies import LanguagesCurrencies
+from .categories_attributes import CategoriesAttributes
