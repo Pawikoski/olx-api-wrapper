6e6d4bf0100b25439bd409f2e09494d165aa69ab
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-06 12:45:46 +0200

    Add Cities and District models

--- olx/models.py
+++ olx/models.py
@@ -39,3 +39,35 @@ class UsersAccountBalance:
     bonus: float
     refund: float
     currency: Optional[str]
+
+
+@dataclass
+class Region:
+    id: int
+    name: str
+
+
+@dataclass
+class City:
+    id: int
+    region_id: int
+    name: str
+    county: str
+    municipality: str
+    latitude: float
+    longitude: float
+
+
+@dataclass
+class District:
+    id: int
+    city_id: int
+    name: str
+    latitude: float
+    longitude: float
+
+
+@dataclass
+class Location:
+    city: City
+    district: Optional[District]
