34b4db3ec17db622001043e396c8a3a26631d8e6
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-06 15:03:42 +0200

    Add import statement for ThreadsMessages

--- olx/__init__.py
+++ olx/__init__.py
@@ -3,3 +3,4 @@ from .users import Users
 from .cities_districts import CitiesDistricts
 from .languages_currencies import LanguagesCurrencies
 from .categories_attributes import CategoriesAttributes
+from .threads_messages import ThreadsMessages
