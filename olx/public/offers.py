1cb4a487d770b70dcaa820df16c3a3cd5e665726
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-08 15:48:25 +0200

    Extend the breadcrumbs method to fetch breadcrumbs for specified category id or offer id

--- olx/public/offers.py
+++ olx/public/offers.py
@@ -35,10 +35,18 @@ class OffersMetadata(OlxPublic):
     def __init__(self) -> None:
         super().__init__()
 
-    def breadcrumbs(self, category_id: int = None):
-        endpoint = "/api/v1/offers/metadata/breadcrumbs/"
+    def breadcrumbs(self, category_id: int = None, offer_id: int = None):
+        assert category_id or offer_id, "Provide category_id or offer_id argument"
+        assert not (
+            category_id and offer_id
+        ), "Provide only one argument: category_id or offer_id"
+
         params = dict()
         if category_id:
-            params["category_id"] = category_id
+            endpoint = "/api/v1/offers/metadata/breadcrumbs/"
+            if category_id:
+                params["category_id"] = category_id
+        else:
+            endpoint = f"/api/v1/offers/{offer_id}/breadcrumbs/"
         response = self.get(endpoint, params=params)
         return from_dict(BreadcrumbResponse, response.json())
