1d18a2894819df10c58f7ac6847c72569e6261d2
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-08 18:24:11 +0200

    Change version to 0.3.1

--- olx/__init__.py
+++ olx/__init__.py
@@ -1,5 +1,5 @@
 __title__ = "olx-api-wrapper"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Paweł Stawikowski"
 __copyright__ = "Copyright (c) 2024 Paweł Stawikowski"
 __license__ = "MIT"
