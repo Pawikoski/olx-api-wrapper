d4fbd32eb3cc9661ac58f9e946cdfa49b8459ffc
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-08 15:29:27 +0200

    Create wrapper Seo with corresponding model

---
+++ olx/public/seo.py
@@ -0,0 +1,31 @@
+from .olx_public import OlxPublic
+from .models.seo.popular_searches import PopularSearchesResponse
+from dacite import from_dict
+
+
+class Seo(OlxPublic):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def popular_searches(
+        self,
+        category_id: int = None,
+        region_id: int = None,
+        city_id: int = None,
+        offset: int = 0,
+        count: int = 10,
+    ):
+        endpoint = "/api/v1/seo/popular-searches/"
+        params = {
+            "category_id": category_id,
+            "offset": offset,
+            "count": count,
+        }
+        if region_id:
+            params["region_id"] = region_id
+        if city_id:
+            params["city_id"] = city_id
+        response = self.get(
+            endpoint, params=params, extra_headers={"accept-language": "pl"}
+        )
+        return from_dict(PopularSearchesResponse, response.json())
