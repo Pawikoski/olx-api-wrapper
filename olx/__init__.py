f2e135c9be829564bb7014fb991f04a52345fe75
Paweł Stawikowski <pawikoski@gmail.com>
2024-05-08 17:41:45 +0200

    Move partner API wrapper files to partners module

--- olx/__init__.py
+++ olx/__init__.py
@@ -4,15 +4,4 @@ __author__ = "Paweł Stawikowski"
 __copyright__ = "Copyright (c) 2024 Paweł Stawikowski"
 __license__ = "MIT"
 
-from .auth import Auth
-from .users import Users
-from .cities_districts import CitiesDistricts
-from .languages_currencies import LanguagesCurrencies
-from .categories_attributes import CategoriesAttributes
-from .threads_messages import ThreadsMessages
-from .paid_features import PaidFeatures
-from .adverts import Adverts
-from .advert_statistics import AdvertsStatistics
-from .advert_logo import AdvertLogo
-from .user_business import UsersBusiness
-from .payments import Payments
+from . import partner
