92b7e9ed90d441e08bfcd5449e1a246000713ab3
Paweł <pawikoski@gmail.com>
2025-04-25 20:27:29 +0200

    Update version to 1.2.0 and enhance setup.py dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 setup(
     name="olx-api-wrapper",
-    version="1.1.0",
+    version="1.2.0",
     description="Unofficial Wrapper for OLX API",
     author="Paweł Stawikowski",
     author_email="pawikoski@gmail.com",
     packages=find_packages(),
     url="https://github.com/Pawikoski/olx-api-wrapper",
-    install_requires=["requests", "dacite"],
+    install_requires=["requests>=2.32.3", "dacite>=1.9.2", "py-moneyed>=3.0"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
@@ -22,4 +21,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.11",
+    keywords="olx api wrapper unofficial",
+    project_urls={
+        "Documentation": "https://github.com/Pawikoski/olx-api-wrapper#readme",
+        "Issue Tracker": "https://github.com/Pawikoski/olx-api-wrapper/issues",
+    },
 )
